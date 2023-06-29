bd947d9ff6c360cf301d6cc3cecb5171a24cdf1d
Belloda <carlos.belloda@inetum.com>
2023-06-29 18:46:19 -0500

    add example

--- modules/feature_selection.py
+++ modules/feature_selection.py
@@ -11,3 +11,7 @@ def feature_selection_prediction(X_train,y_train):
   plt.show()
   return rf.feature_importances_
 
+#Only for numeric columns
+#X_train= df[info['numeric_columns']].loc[:, df[info['numeric_columns']].columns != 'Salary']
+#y_train = df['Salary']
+#fea.feature_selection_prediction(X_train,y_train)
